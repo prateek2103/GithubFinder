d478664cf9160db82618fe9d9e4054e7517560fe
prateek2103 <prateek007.purohit@gmail.com>
2022-03-21 00:50:17 +0530

    error handling left

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl">Github Finder</h1>
+      <p className="text-lg">
+        A basic react project to find github users and see information about
+        them.
+      </p>
+    </div>
+  );
+}
+
+export default About;
