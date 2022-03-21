0c24bd37218f153d6796b9e044488c2956151b8d
prateek2103 <prateek007.purohit@gmail.com>
2022-03-21 14:14:49 +0530

    readme and about updated

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -3,10 +3,13 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1 className="text-6xl">Github Finder</h1>
-      <p className="text-lg">
+      <h1 className="text-6xl py-10">Github Finder</h1>
+      <p className="text-lg py-2">
         A basic react project to find github users and see information about
         them.
+      </p>
+      <p className="text-lg">
+        It retrieves data about the users via Github API.
       </p>
     </div>
   );
